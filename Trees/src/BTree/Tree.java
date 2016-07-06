64e4a5251b4b5d261f7fcd64e44308d4bfcd24db
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2016-07-06 19:45:31 -0400

    Updated function declarations of findHeight, depthOfNode, insertNode.

--- Trees/src/BTree/Tree.java
+++ Trees/src/BTree/Tree.java
@@ -5,11 +5,11 @@ public interface Tree
     //public boolean isRoot();
     public boolean isTreeEmpty();
 
-    public int findHeight();
-    public int depthOfNode(Node curr);
+    public int findHeight(Node curr, int height);
+    public int depthOfNode(Node curr, int dt, int depth);
     public int numChildren(Node curr);//returns # of children a node has
 
-    public void insertNode(int dt);
+    public void insertNode(Node curr, int dt);
     public Node searchNode(Node curr, int dt);
     public void deleteNode(int dt);
     public void removeTree(Node curr);
