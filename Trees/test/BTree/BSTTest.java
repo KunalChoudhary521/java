1880d0484ccdb3dc89872736de90b6a72355f204
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2016-07-06 12:21:18 -0400

    Created a testing unit for BSTree class.

---
+++ Trees/test/BTree/BSTTest.java
@@ -0,0 +1,69 @@
+package BTree;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+//Docs: https://github.com/junit-team/junit4/wiki/Getting-started
+
+public class BSTTest
+{
+    @Test
+    public void isTreeEmpty() throws Exception
+    {
+
+    }
+
+    @Test
+    public void findHeight() throws Exception
+    {
+
+    }
+
+    @Test
+    public void depthOfNode() throws Exception
+    {
+
+    }
+
+    @Test
+    public void numChildren() throws Exception
+    {
+
+    }
+
+    @Test
+    public void insertNode() throws Exception
+    {
+
+    }
+
+    @Test
+    public void searchNode() throws Exception
+    {
+
+    }
+
+    @Test
+    public void deleteNode() throws Exception
+    {
+
+    }
+
+    @Test
+    public void removeTree() throws Exception
+    {
+
+    }
+
+    @Test
+    public void largestFromLeft() throws Exception
+    {
+
+    }
+
+    @Test
+    public void inOrderTraversal() throws Exception
+    {
+
+    }
+
+}
