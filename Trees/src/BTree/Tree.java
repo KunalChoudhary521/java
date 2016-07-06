f32a07a182c96bb4be1257df132c26d3c98c8e26
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2016-07-06 12:17:06 -0400

    Updated definitions on tree functions.

--- Trees/src/BTree/Tree.java
+++ Trees/src/BTree/Tree.java
@@ -4,14 +4,13 @@ public interface Tree
 {
     //public boolean isRoot();
     public boolean isTreeEmpty();
-    public boolean isLeafNode(Node curr);
 
     public int findHeight();
     public int depthOfNode(Node curr);
     public int numChildren(Node curr);//returns # of children a node has
 
     public void insertNode(int dt);
-    public Node searchNode(int dt);
+    public Node searchNode(Node curr, int dt);
     public void deleteNode(int dt);
-    public void removeTree();
+    public void removeTree(Node curr);
 }
