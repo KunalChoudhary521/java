557838332c93f2e8151a7f29c35a68b3e07976dd
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2016-07-06 21:49:32 -0400

    Created AVLTree class.

---
+++ Trees/src/BalancedTree/AVLTree.java
@@ -0,0 +1,15 @@
+package BalancedTree;
+import BTree.BST;
+
+public class AVLTree extends BST implements BalancedInterface
+{
+    public AVLTree()
+    {
+        super();
+    }
+
+    public void Balance()
+    {
+
+    }
+}
